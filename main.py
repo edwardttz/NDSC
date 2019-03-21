bc6cd93ad8021204db44f9aaea0a8f8d8cc51bad
Edward Tan <tan.teck.zhi@gmail.com>
2019-03-21 23:50:44 +0800

    Update of model decoupling Part 1 - to reduce memory usage

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 import sys
 import datetime
 
-import helper
 import modelTrainer
 import dataProcessor
 
 # To train data into model, run this:
-# python main.py Train train.csv
+# python main.py Train train.csv <number>
+# 0: Train TF Model; 1: Prepare TF Model; 2: Train BOW Model
 #
 # To generate output from test data, run this:
 # python main.py Validate test.csv experimental_result.csv
 
 def main():
-	action = sys.argv[1]
-	action = action.lower()
+	action = sys.argv[1].lower()
 	start_time = datetime.datetime.now()
-	if(action.__eq__('train') and len(sys.argv) == 3):
-		inputFile = sys.argv[2]
-		input_np = helper.readCsvToNumpy(inputFile)
+
+	if(action.__eq__('train') and len(sys.argv) == 4):
 		# Training model starts here
-		modelTrainer.trainModel(input_np)
+		modelTrainer.trainModel(sys.argv[2], int(sys.argv[3]))
 
 		# Calculation of total time taken
 		print("Start Time: " + str(start_time))
@@ -28,14 +26,8 @@ def main():
 		print("Complete training model")
 		# Training model ends here
 	elif(action.__eq__('validate') and len(sys.argv) == 4):
-		inputFile = sys.argv[2]
-		outputFile = sys.argv[3]
-		input_np = helper.readCsvToNumpy(inputFile)
 		# Process test data starts here
-		output_np = dataProcessor.processTestData(input_np)
-		print("Complete processing data")
-		# Process test data ends here
-		helper.saveResultToCsv(output_np, outputFile)
+		dataProcessor.processTestData(sys.argv[2], sys.argv[3])
 
 		# Calculation of total time taken
 		print("Start Time: " + str(start_time))
@@ -44,13 +36,9 @@ def main():
 		print("Attempted to save into csv")
 	else:
 		print("I am sad to say")
-		print(".")
-		print(".")
-		print(".")
-		print(".")
 		print("You did not feed me well.")
 		print("If you want to train me, you must train me with passion + a file name")
-		print("Sample: \"python main.py TRAIN train.csv\"")
+		print("Sample: \"python main.py TRAIN train.csv 2\"")
 		print("If you want me to think for your lazy mind, you need to give me 1 more filename")
 		print("For example: \"python main.py VALIDATE test.csv result.csv")
 
